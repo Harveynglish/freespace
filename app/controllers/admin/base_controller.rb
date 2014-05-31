271b7179a9395339855df1f10ca67063c3e2db31
Lora L. Abe <lora.abe@gmail.com>
2014-05-31 17:57:14 -0500

    adds controllers and routes for admin stuff

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,11 @@
+class Admin::BaseController < ApplicationController
+  # before_filter :require_admin
+
+  private
+    def require_admin
+      unless current_user.is_admin?
+        flash.now[:alert] = "You must be an admin to access that page."
+        redirect_to root_path
+      end
+    end
+end
---
+++ app/controllers/admin/spaces_controller.rb
@@ -0,0 +1,22 @@
+class Admin::SpacesController < Admin::BaseController
+  def index
+  end
+  
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
