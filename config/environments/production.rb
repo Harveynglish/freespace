0cc4548cfbb37f617fe6943fef6cba89bceafeeb
Taaha Chaudhry <t44h413@gmail.com>
2014-05-31 18:47:01 +0000

    Set up user with devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Freespaceatx::Application.configure do
+  config.action_mailer.default_url_options = { :host => '' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
