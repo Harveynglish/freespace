67e1db34f75c5db1e662720a0df41f356e576c23
Taaha Chaudhry <t44h413@gmail.com>
2014-05-31 18:13:17 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Freespaceatx::Application.config.secret_key_base = '323752efa398be014484b71f19b17e2d7ba615dd88eaef3d134318ee0f364a95e97b0cd3986ae50343592a2f1fa64b07e9c38c2471473f1c58be4ca8627f9754'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Freespaceatx::Application.config.session_store :cookie_store, key: '_freespaceatx_session'
