99404fc237a91fa2622dafa8be15c63f64e725d6
Lora L. Abe <lora.abe@gmail.com>
2014-06-01 11:21:07 -0500

    merge conflict

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,8 +1,16 @@
 class Admin::EventsController < Admin::BaseController
   def index
+    if @only_unplaced = params[:only_unplaced] == "true"
+      @events = Event.includes(:activity, :user).unplaced
+    elsif @only_placed = params[:only_placed] == "true"
+      @events = Event.includes(:activity, :user, :space).placed
+    else
+      @events = Event.all
+    end
   end
 
   def edit
+    @event = Event.find(params[:id])
     # if no space_id is assigned
       # show partial that lists available spaces
     # else
