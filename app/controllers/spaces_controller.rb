a36b267c3cce9946920198e61a5e1962bd31083b
Kathryn Beaty <kabeaty@gmail.com>
2014-06-01 14:27:54 -0500

    Add fields to activity edit form

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,5 +1,5 @@
 class SpacesController < ApplicationController
-  # before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 
   def new
     @space = Space.new
