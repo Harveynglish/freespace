250f9fc54bf456cdb25b55cf6a8846eb71323a4f
Taaha Chaudhry <t44h413@gmail.com>
2014-06-01 11:48:43 -0500

    Created user dashboard of spaces and activities- my accout tab

--- db/schema.rb
+++ db/schema.rb
@@ -20,18 +20,20 @@ ActiveRecord::Schema.define(version: 20140601152124) do
     t.integer  "user_id"
     t.string   "name"
     t.text     "description"
-    t.integer  "capacity"
-    t.boolean  "volunteers_needed"
+    t.integer  "capacity_id"
     t.text     "comments"
-    t.boolean  "alternate_contact"
-    t.string   "contact_name"
-    t.string   "contact_organization"
-    t.string   "contact_phone"
-    t.string   "contact_email"
-    t.text     "admin_notes"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "when"
+  end
+
+  create_table "availabilities", force: true do |t|
+    t.integer  "space_id"
+    t.integer  "day_id"
+    t.time     "start_time"
+    t.time     "end_time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "available_date_ranges", force: true do |t|
@@ -42,13 +44,8 @@ ActiveRecord::Schema.define(version: 20140601152124) do
     t.datetime "updated_at"
   end
 
-  create_table "available_times", force: true do |t|
-    t.integer  "space_id"
-    t.integer  "day_id"
-    t.time     "start_time"
-    t.time     "end_time"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "capacities", force: true do |t|
+    t.string "range"
   end
 
   create_table "days", force: true do |t|
@@ -58,8 +55,9 @@ ActiveRecord::Schema.define(version: 20140601152124) do
   create_table "events", force: true do |t|
     t.integer  "space_id"
     t.integer  "activity_id"
-    t.datetime "start_time"
-    t.datetime "end_time"
+    t.date     "date"
+    t.time     "start_time"
+    t.time     "end_time"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -73,17 +71,10 @@ ActiveRecord::Schema.define(version: 20140601152124) do
     t.string   "state"
     t.string   "zip"
     t.text     "description"
-    t.integer  "capacity"
+    t.integer  "capacity_id"
     t.text     "comments"
     t.float    "latitude"
     t.float    "longitude"
-    t.boolean  "always_available"
-    t.boolean  "alternate_contact"
-    t.string   "contact_name"
-    t.string   "contact_organization"
-    t.string   "contact_phone"
-    t.string   "contact_email"
-    t.text     "admin_notes"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -99,13 +90,14 @@ ActiveRecord::Schema.define(version: 20140601152124) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+    t.boolean  "is_admin"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "name"
-    t.boolean  "is_admin"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
